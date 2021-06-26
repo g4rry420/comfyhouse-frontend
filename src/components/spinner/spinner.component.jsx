22769cf43969124151fd696f8a1d07879a1c55a2
gurkiran_singh <gurkiransinghk@gmail.com>
2021-06-26 04:13:14 -0400

    Added spinner text for waiting
    Signed-off-by: gurkiran_singh <gurkiransinghk@gmail.com>

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -4,10 +4,12 @@ import "./spinner.styles.css"
 
 export default function Spinner() {
     return (
+        <>
         <div className="spinner-overlay">
             <div className="spinner-container">
             </div>
-            <p>The backend is hosted in Glitch.So, it might take a minute to load. Please, wait. Thank You</p>
         </div>
+        <p>The backend is hosted in Glitch.So, it might take a minute to load. Please, wait. Thank You</p>
+        </>
     )
 }
